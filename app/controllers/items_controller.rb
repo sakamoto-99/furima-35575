4d7e22996f9072868b05c5da3a5c5d78cc4d4c6d
sakamoto-99 <neo.sasa.4423@gmail.com>
2021-05-17 17:22:19 +0900

    コメント内容の修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_items, only: [:show, :edit, :update, :destroy]
-  before_action :redirect_to_root, only: [:edit, :update]
+  before_action :redirect_to_root, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
