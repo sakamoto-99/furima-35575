4a190938db814956bf8d216b34ffc24db09daaa6
sakamoto-99 <neo.sasa.4423@gmail.com>
2021-05-25 10:50:55 +0900

    挙動確認の修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   with_options presence: true do
     validates :nickname
     validates :birthday
@@ -11,6 +12,5 @@ class User < ApplicationRecord
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     validates :first_name_ruby, format: { with: /\A[ァ-ヶー]+\z/ }
     validates :last_name_ruby, format: { with: /\A[ァ-ヶー]+\z/ }
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   end
 end
